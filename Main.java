9dc7a1ddee566dbdee3354ed11b68c08e1f46763
nazarevitsch <45229160+nazarevitsch@users.noreply.github.com>
2019-05-14 23:29:44 +0300

    Main Class

---
+++ Main.java
@@ -0,0 +1,14 @@
+package oop4;
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        //File file = new File("tru256.BMP");
+//        File file = new File("MARBLES.BMP");
+//        ImageBearbeiten i = new ImageBearbeiten(file);
+//        i.reseizX(2);
+
+        ImageBearbeiten i = new ImageBearbeiten();
+    }
+}
